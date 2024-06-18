d18cde6119969b7519aa000dcdefd6655726a5d4
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-06-18 13:24:37 +0100

    Add user authentication for forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
+login.login_view = 'login'
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -15,6 +18,7 @@ def create_app(config_class=DevelopmentConfig):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
     with app.app_context():
         # Register blueprints
         from .user_api import user_api_blueprint
